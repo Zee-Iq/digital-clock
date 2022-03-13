9b06169c8a2d4ab172c2e251631ea7cfe56f1671
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-03-13 20:20:56 +0100

    added useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
- import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 const App = () => {
-
   let time = new Date().toLocaleTimeString();
 
-  const [currentTime, setTime] = useState(time)
+  const [currentTime, setTime] = useState(time);
 
   const updateTime = () => {
     let time = new Date().toLocaleTimeString();
-    setTime(time)
-  } 
-  
+    setTime(time);
+  };
+
+  setInterval(updateTime, 1000);
+
   return (
     <>
-      <h1>{currentTime}</h1>
-      <button onClick={updateTime}>Click me to get current time</button>
+      <h1 className="App">{currentTime}</h1>
     </>
-  )
-}
+  );
+};
 
 export default App;
